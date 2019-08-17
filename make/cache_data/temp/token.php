dacb1090d6856fc83c23cda4f921ca1a60d10263
fuyangyu <314677335@qq.com>
2019-08-17 19:02:46 +0800

    接口文档 框架核心文件

---
+++ make/cache_data/temp/token.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  12 => 
+  array (
+    'token' => '0fadb03b5e90e48acbe97efea33582b2',
+    'time' => 1546097516,
+    'uid' => 12,
+  ),
+  11 => 
+  array (
+    'token' => '4f181f9aebfc3166998a1d7e1a7e4de0',
+    'time' => 1546097567,
+    'uid' => 11,
+  ),
+  24 => 
+  array (
+    'token' => '4a879d7f44378bb077db03350bcd105f',
+    'time' => 1546125051,
+    'uid' => 24,
+  ),
+  28 => 
+  array (
+    'token' => '106b1afd2814f97eda6acce76709b21f',
+    'time' => 1546132309,
+    'uid' => '28',
+  ),
+  29 => 
+  array (
+    'token' => '1217020ac841335d681cc64ad0350f01',
+    'time' => 1546141729,
+    'uid' => '29',
+  ),
+  30 => 
+  array (
+    'token' => '289d76138cbef016dac77868219d2b22',
+    'time' => 1546146554,
+    'uid' => '30',
+  ),
+  31 => 
+  array (
+    'token' => 'c3254e7d9d4e8dfbc51695a0718fe360',
+    'time' => 1546263471,
+    'uid' => '31',
+  ),
+  27 => 
+  array (
+    'token' => 'b67d58bb0925be88c2103dffc17c1196',
+    'time' => 1546441642,
+    'uid' => 27,
+  ),
+  32 => 
+  array (
+    'token' => 'd5289a5cbff9d6072d76bdf6bdfe65b0',
+    'time' => 1546506278,
+    'uid' => '32',
+  ),
+);
+?>
